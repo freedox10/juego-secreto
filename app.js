4441370fd79e68d79d8396d659c274cc4af464d7
freedox10 <alfre2636@yahoo.com.ar>
2024-01-23 01:37:09 -0300

    LogicaProgramacion1-Aula4-BuenasPracticas

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-/*
 //Variables
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
+
+console.log(numeroSecreto);
 
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
 
-    console.log(numeroUsuario);
+    console.log(typeof(numeroUsuario));
     if (numeroUsuario == numeroSecreto) {
         //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
@@ -19,42 +21,16 @@ while (numeroUsuario != numeroSecreto) {
             alert('El número secreto es mayor');
         }
         //Incrementamos el contador cuando no acierta
-        intentos = intentos + 1;
-        palabraVeces = 'veces';
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
+
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
         //La condición no se cumplió
         //alert('Lo siento, no acertaste el número');
     }
 }
-*/
-/*
-let contador = 1;
-while (contador <= 10) {
-console.log(contador);
-contador++;
-}
-console.log(`Ya contamos hasta ${contador - 1}`);
-*/
-/*
-let contador = 10;
-while (contador >= 0) {
-console.log(contador);
-contador--;
-}
-console.log(`Ya contamos hasta ${contador + 1}`);
-*/
-/*
-let numeroMaximo = prompt("Ingresa un número para la cuenta Regresiva:");
-while (numeroMaximo >= 0) {
-console.log(numeroMaximo);
-numeroMaximo--;
-}
-console.log(`Llegamos a ${numeroMaximo + 1}`);
-*/
-
-let numeroMaximo = prompt("Ingresa un número para la cuenta Progresiva:");;
-let contador = 0;
-while (contador <= numeroMaximo) {
-console.log(contador);
-contador++
-}
-console.log(`Ya contamos hasta ${contador - 1}`);
